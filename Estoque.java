0870d581bbaff1a90663fd92158864f75540cc13
Mateus Jordan <matjordan000@gmail.com>
2025-10-01 14:47:59 -0300

    Captalizer

--- Estoque.java
+++ Estoque.java
@@ -35,7 +35,73 @@ public class Estoque
         return true;
     }
 
-    
+    public void listarprodutos ()
+    {
+        if (produtos.isEmpty())
+        {
+            System.out.println ("nenhum produto cadastrado.");
+        }
+        else
+        {
+            for (Produto p : produtos values())
+        }
+        System.out.println(p);
+    }
 
+    public boolean salvarEstoque (String nomearquivo)
+    {
+        File arquivo = new File (nomearquivo)
+
+        if (arquivo.exists())
+        {
+            System.out.println ("arquivo ja existentes.");
+            return false
+        }
+        
+        try (BufferWrirter wirter = new BufferWrirter (new FileWirter(arquivo)))
+        {
+            for (produto p : produto.values())
+            {
+                wirter.wirte(p.toFileString());
+                wirte.newLine();
+            }
+            return false;
+        }
+        catch (IOExeption e)
+        {
+            System.out.println ("erro ao salvar o arquivo: " + e.getMessage);
+            return false;
+        }
+
+    }
+
+        public boolean carregarEstoque (String nomearquivo)
+        {
+            File arquivo = new File (nomearquivo);
+
+            if (!arquivo.exists())
+            {
+                System.out.println ("arquivo nao encontrado.");
+                return false;
+            }
+
+            try (BufferedReader reader = new BufferedReader (new FileReader(arquivo)))
+        {
+            String linha;
+            while((linha = reader.readLine()) != null)
+        }
+         produto p = Produto.fromFileString (linha);
+
+         if (p != null)
+         {
+            produtos.put (p.getcodigo(), p);
+         }
+         return true;
+
+        catch (IOExeption e)
+        {
+            System.out.println ("erro ao carregar o arquivo: " + e.getMessage);
+            return false;
+        }
 
 }
